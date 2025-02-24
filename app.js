450074cfec33d462514b7c371221692266882140
Guillermo Seron <guillermoseron@gmail.com>
2025-02-24 19:11:44 -0300

    Agrega función 'sortearAmigo()'

--- app.js
+++ app.js
@@ -30,3 +30,17 @@ function listaAmigos() {
     return;
 }
 
+function sortearAmigo() {
+    //Inicia limpiando el elemento de HTML para evitar duplicados o resultados de ejecuciones anteriores;
+amigoSecreto.innerHTML = "";
+if (amigos.length <= 0) { //Si la lista de nombres de amigos está vacía, lanza un alerta;
+    alert("ERROR. El sorteo no puede comenzar si no ingresa nombres");
+    return;
+} else {
+    //Genera un número aleatorio que no puede ser mayor a la cantidad de nombres válidos ingresados;
+    let numeroAmigo = Math.floor(Math.random()*amigos.length);
+    //Muestra el nombre del amigo en la posición aleatoria de la lista 'amigos' determinada anteriormente;
+    amigoSecreto.innerHTML = `Su amigo secreto es ${amigos[numeroAmigo]}`;
+    return;
+}
+}
